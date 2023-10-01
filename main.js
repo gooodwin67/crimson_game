9a7fef6777a1f56438328fae07a9f87fd5e6e1f0
gooodwin67 <sn67@inbox.ru>
2023-10-01 20:13:20 +0300

    enemies

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
 import { movePlayer } from './player.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { detectCollisionCubes } from './detectColisions.js';
@@ -11,11 +12,14 @@ let playerLoaded = false;
 let worldLoaded = false;
 
 let clock = new THREE.Clock();
+let stats;
 
 let light;
 let mouse = new THREE.Vector2();
 let raycaster = new THREE.Raycaster();
-const materialLine = new THREE.LineBasicMaterial( { color: 0xff00ff } );
+const materialLine = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+const materialLine2 = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const materialLine3 = new THREE.LineBasicMaterial( { color: 0x000000 } );
 
 let plane, player;
 
@@ -26,6 +30,7 @@ let playerBox = new THREE.Group();
 let clock2 = new THREE.Clock();
 
 let enemy;
+let enemyBox = new THREE.Group();
 let enemies;
 
 const raycasterEnemy = new THREE.Raycaster();
@@ -49,6 +54,8 @@ window.addEventListener("resize", (event) => {
   camera.updateProjectionMatrix();
   renderer.setSize(innerWidth, innerHeight);
 });
+stats = new Stats();
+document.body.appendChild( stats.dom );
 
 let camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 10000);
 camera.position.set(0, 150, 0);
@@ -58,7 +65,7 @@ var ambient = new THREE.AmbientLight( 0xffffff );
 scene.add( ambient )
 light = new THREE.PointLight( 0xffffff, 0.7, 700);
 light.position.set(50,50, 100);
-light.castShadow = true;
+//light.castShadow = true;
 scene.add( light );
 
 let controls = new OrbitControls(camera, renderer.domElement);
@@ -134,6 +141,7 @@ init();
 renderer.setAnimationLoop((_) => {
     
     animate();
+    stats.update();
     renderer.render(scene, camera);
 });
 
@@ -370,17 +378,47 @@ function addEnemy() {
   const geometryEnemy = new THREE.BoxGeometry(10,10,10)
   const materiaEnemy = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
   let enemyBody = new THREE.Mesh( geometryEnemy, materiaEnemy);
+  enemyBody.name = 'enemyBody';
 
   const geometryEnemyFront = new THREE.BoxGeometry(2,10,2)
   const materiaEnemyFront = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
   let enemyFront= new THREE.Mesh( geometryEnemyFront, materiaEnemyFront);
 
 
-  const geometryenemyBoxTop = new THREE.BoxGeometry(16,30,8);
-  const materialenemyBoxTop = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+  
+  enemyBox.name = 'enemyBox';
+  const geometryenemyBoxLeft = new THREE.BoxGeometry(1,15,2);
+  const materialenemyBoxLeft = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+  const enemyBoxLeft = new THREE.Mesh( geometryenemyBoxLeft, materialenemyBoxLeft);
+  enemyBoxLeft.name = 'enemyBoxLeft';
+  enemyBoxLeft.position.set(4,0,0);
+  enemyBoxLeft.rotation.y = Math.PI/2;
+  enemyBox.add( enemyBoxLeft );
+
+  const geometryenemyBoxRight = new THREE.BoxGeometry(1,15,2);
+  const materialenemyBoxRight = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+  const enemyBoxRight = new THREE.Mesh( geometryenemyBoxRight, materialenemyBoxRight);
+  enemyBoxRight.name = 'enemyBoxRight';
+  enemyBoxRight.position.set(-4,0,0);
+  enemyBoxRight.rotation.y = Math.PI/2;
+  enemyBox.add( enemyBoxRight );
+
+  const geometryenemyBoxTop = new THREE.BoxGeometry(1,15,2);
+  const materialenemyBoxTop = new THREE.MeshPhongMaterial( { color: 0x000000 } );
   const enemyBoxTop = new THREE.Mesh( geometryenemyBoxTop, materialenemyBoxTop);
   enemyBoxTop.name = 'enemyBoxTop';
-  enemyBoxTop.position.set(0,0,5);
+  enemyBoxTop.position.set(0,0,4);
+  enemyBox.add( enemyBoxTop );
+
+  const geometryenemyBoxBottom = new THREE.BoxGeometry(1,15,2);
+  const materialenemyBoxBottom = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+  const enemyBoxBottom = new THREE.Mesh( geometryenemyBoxBottom, materialenemyBoxBottom);
+  enemyBoxBottom.name = 'enemyBoxBottom';
+  enemyBoxBottom.position.set(0,0,-4);
+  enemyBox.add( enemyBoxBottom );
+
+
+
   
 
 
@@ -390,23 +428,65 @@ function addEnemy() {
   enemy = new THREE.Group();
   enemy.add( enemyBody );
   enemy.add( enemyFront );
+  enemy.add( enemyBox );
   enemyFront.position.set(0,0,10);
 
   enemies = [];
   enemies.push(enemy.clone());
   enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+  enemies.push(enemy.clone());
+
   enemies.forEach((el)=>{
     el.userData.speed = Math.random();
     el.userData.raycaster = new THREE.Raycaster();
+    el.userData.seeRaycaster = new THREE.Raycaster();
+    el.userData.idleRaycaster = new THREE.Raycaster();
     el.userData.rayLine = new THREE.Line( new THREE.BufferGeometry(), materialLine);
+    el.userData.rayLine2 = new THREE.Line( new THREE.BufferGeometry(), materialLine2);
+    el.userData.rayLine3 = new THREE.Line( new THREE.BufferGeometry(), materialLine3);
     el.userData.rayLine.frustumCulled = false;
     el.userData.rayLine.position.set(0,5,0);
+    el.userData.rayLine2.frustumCulled = false;
+    el.userData.rayLine2.position.set(0,5,0);
+    el.userData.rayLine3.frustumCulled = false;
+    el.userData.rayLine3.position.set(0,5,0);
     el.userData.direction = new THREE.Vector3();
     el.userData.far = new THREE.Vector3();
-    el.userData.angle = 0;
+    el.userData.angle = ['top', 'down', 'left', 'right'];
     el.userData.turn = 'top';
-    el.userData.canWalk = true;
+    el.userData.idle = 'true';
+    
     el.userData.enemyBoxTop = enemyBoxTop.clone();
+
+    el.userData.seePlayer = false;
+    el.userData.hearPlayer = false;
 
     el.add(enemy.userData.enemyBoxTop);
 
@@ -414,78 +494,200 @@ function addEnemy() {
 
     scene.add( el );
     scene.add( el.userData.rayLine );
-    scene.add( el.userData.enemyBoxTop );
+    scene.add( el.userData.rayLine2 );
+    scene.add( el.userData.rayLine3 );
   });
 
 
   enemies[0].position.set(90, 0, 50);
   enemies[1].position.set(90, 0, 0);
+  enemies[2].position.set(120, 0, 0);
+  enemies[3].position.set(40, 0, 100);
+  enemies[4].position.set(100, 0, 0);
+  enemies[5].position.set(90, 0, 50);
+  enemies[6].position.set(90, 0, 0);
+  enemies[7].position.set(120, 0, 0);
+  enemies[8].position.set(40, 0, 100);
+  enemies[9].position.set(100, 0, 0);
+  enemies[10].position.set(90, 0, 50);
+  enemies[11].position.set(90, 0, 0);
+  enemies[12].position.set(120, 0, 0);
+  enemies[13].position.set(40, 0, 100);
+  enemies[14].position.set(100, 0, 0);
+  enemies[15].position.set(90, 0, 50);
+  enemies[16].position.set(90, 0, 0);
+  enemies[17].position.set(120, 0, 0);
+  enemies[18].position.set(40, 0, 100);
+  enemies[19].position.set(100, 0, 0);
+  enemies[0].position.set(90, 0, 50);
+  enemies[21].position.set(90, 0, 0);
+  enemies[22].position.set(120, 0, 0);
+  enemies[23].position.set(40, 0, 100);
+  enemies[24].position.set(100, 0, 0);
+  enemies[25].position.set(90, 0, 50);
+  enemies[26].position.set(90, 0, 0);
+  enemies[27].position.set(120, 0, 0);
+  enemies[28].position.set(40, 0, 100);
+  enemies[29].position.set(100, 0, 0);
 
 }
 
 function fightEnemies() {
-  enemy.userData.canWalk = true;
   enemies.forEach(element => {
-    if (element.position.distanceTo(player.position) < 180) {
-      if (!element.userData.intersectWorld) {
-        element.position.add(player.position.clone().sub(element.position).normalize().multiplyScalar(/*element.userData.speed*/0.6));
-        element.lookAt(player.position);
-      }
-      element.userData.intersectWorld = false;
+    element.userData.hearPlayer = true;
+
+    
+    if (element.position.distanceTo(player.position) < 80) {
+      element.userData.hearPlayer = true;
+      element.userData.idle = false;
+    }
+    else {
+      element.userData.hearPlayer = false;
+      
     }
 
-    element.userData.raycaster.set(player.position, element.userData.direction.subVectors(element.position, player.position).normalize());
-    element.userData.raycaster.far = new THREE.Vector3().subVectors(element.position, player.position).length();
 
-    element.userData.rayLine.geometry.setFromPoints(
-      [
-        player.position,
-        element.position,
-      ]
-    );
+    if (element.userData.hearPlayer && element.userData.seePlayer && !element.userData.idle) {
+      element.userData.raycaster.set(player.position, element.userData.direction.subVectors(element.position, player.position).normalize());
+      element.userData.raycaster.far = new THREE.Vector3().subVectors(element.position, player.position).length();
+
+      element.userData.seeRaycaster.set(player.position, element.userData.direction.subVectors(element.position, player.position).normalize());
+      element.userData.seeRaycaster.far = new THREE.Vector3().subVectors(element.position, element.userData.raycaster.ray.origin).length();
+    }
+    else if (element.userData.hearPlayer && !element.userData.seePlayer && !element.userData.idle) {
+      element.userData.seeRaycaster.set(player.position, element.userData.direction.subVectors(element.position, player.position).normalize());
+      element.userData.seeRaycaster.far = new THREE.Vector3().subVectors(element.position, element.userData.seeRaycaster.ray.origin).length();
+    }
+
+
+    // if (!element.userData.idle) {
+    //   element.userData.rayLine.geometry.setFromPoints(
+    //     [
+    //       element.position,
+    //       element.userData.raycaster.ray.origin
+    //     ]
+    //   );
+    //   element.userData.rayLine2.geometry.setFromPoints(
+    //     [
+    //       element.position,
+    //       element.userData.seeRaycaster.ray.origin
+    //     ]
+    //   );
+    // }
+    // else {
+    //   element.userData.rayLine3.geometry.setFromPoints(
+    //     [
+    //       element.position,
+    //       element.userData.idleRaycaster.ray.origin
+    //     ]
+    //   );
+    // }
+
     
 
-    element.userData.angle = element.getWorldDirection(new THREE.Vector3());
-    if (element.userData.angle.x <= 0.6 && element.userData.angle.x >= -0.6 && element.userData.angle.z >= 0.6) {
-      element.userData.turn = 'top';
+
+    //element.userData.intersectWorld = false;
+
+    if (element.userData.seePlayer && element.userData.hearPlayer && !element.userData.idle) {
+      element.position.add(element.userData.raycaster.ray.origin.clone().sub(element.position).normalize().multiplyScalar(element.userData.speed));
+      element.children.filter(el => el.name == 'enemyBody')[0].lookAt(element.userData.raycaster.ray.origin);
     }
-    else if(element.userData.angle.x <= 0.6 && element.userData.angle.z <= 0.6 && element.userData.angle.z >= -0.6) {
-      element.userData.turn = 'right';
+    else if (!element.userData.seePlayer && element.userData.hearPlayer && !element.userData.idle){
+      element.position.add(element.userData.raycaster.ray.origin.clone().sub(element.position).normalize().multiplyScalar(element.userData.speed));
+      element.children.filter(el => el.name == 'enemyBody')[0].lookAt(element.userData.raycaster.ray.origin);
     }
-    else if(element.userData.angle.x >= -0.6 && element.userData.angle.x <= 0.6 && element.userData.angle.z <= -0.6) {
-      element.userData.turn = 'down';
+    else if (!element.userData.hearPlayer && !element.userData.idle){
+      element.position.add(element.userData.raycaster.ray.origin.clone().sub(element.position).normalize().multiplyScalar(element.userData.speed));
+      element.children.filter(el => el.name == 'enemyBody')[0].lookAt(element.userData.raycaster.ray.origin);
     }
-    else if(element.userData.angle.x >= 0.6 && element.userData.angle.z <= 0.6 && element.userData.angle.z >= -0.6) {
-      element.userData.turn = 'left';
+    
+
+    
+    if (element.position.distanceTo(element.userData.raycaster.ray.origin) < 4) {
+      element.userData.idle = true;
+      
     }
 
+
+    
+    element.userData.seePlayer = true;
     city.children.forEach(function(item, index, array) {
-      // if (item.name.indexOf('Cube0') >= 0 && element.userData.raycaster.intersectObject(item).length > 0) {
-      //   element.userData.intersectWorld = true;
-      // }
+      
+      if (item.name.indexOf('Cube00') >= 0) {
 
-      if (item.name.indexOf('Cube0') >= 0 && detectCollisionCubes(element.children[0], item)) {
-          if (element.userData.turn == 'top') {
-            element.position.z = element.position.z-0.6;
-          }
-          if (element.userData.turn == 'down') {
-            element.position.z = element.position.z+0.6;
-          }
-          if (element.userData.turn == 'left') {
-            element.position.x = element.position.x-0.6;
-          }
-          else if (element.userData.turn == 'right') {
-            element.position.x = element.position.x+0.6;
-          }
+        if (element.userData.raycaster.intersectObject(item).length > 0 && element.userData.seeRaycaster.intersectObject(item).length > 0) {
+          element.userData.seePlayer = false;
+        } 
+        
       }
+      
+      if (element.userData.idle) {
+        let timeIdleTurn = Math.random();
+        let idleTurn = new THREE.Vector3(element.position.x, element.position.y, element.position.z);
+        if (timeIdleTurn > 0.999) {
+          element.userData.turn = element.userData.angle[randomIntFromInterval(0,3)];
+          
+        }
+        if (element.userData.turn == 'top') {
+          idleTurn = new THREE.Vector3(element.position.x, element.position.y, element.position.z + element.userData.speed/10);
+        }
+        else if (element.userData.turn == 'down') {
+          idleTurn = new THREE.Vector3(element.position.x, element.position.y, element.position.z - element.userData.speed/10);
+        }
+        else if (element.userData.turn == 'left') {
+          idleTurn = new THREE.Vector3(element.position.x+element.userData.speed/10, element.position.y, element.position.z);
+        }
+        else if (element.userData.turn == 'right') {
+          idleTurn = new THREE.Vector3(element.position.x-element.userData.speed/10, element.position.y, element.position.z);
+        }
+        
+        
+  
+        element.userData.idleRaycaster.set(idleTurn, element.position.clone().normalize());
+        element.children.filter(el => el.name == 'enemyBody')[0].lookAt(element.userData.idleRaycaster.ray.origin);
+
+        // element.userData.idleRaycaster.far = new THREE.Vector3().subVectors(element.position, new THREE.Vector3(element.position.x-20, element.position.y, element.position.z)).length();
+
+        element.position.add(element.userData.idleRaycaster.ray.origin.clone().sub(element.position).normalize().multiplyScalar(element.userData.speed/15));
+        //console.log(element.userData.idle);
+      }
+
+      
+
+
+        if (item.name.indexOf('Cube0') >= 0 && detectCollisionCubes(element.children.filter(el => el.name == 'enemyBox')[0].children.filter(el => el.name == 'enemyBoxLeft')[0], item)) {
+          element.position.x -= element.userData.speed;
+          element.userData.turn = element.userData.angle[randomIntFromInterval(0,3)];
+        };
+        if (item.name.indexOf('Cube0') >= 0 && detectCollisionCubes(element.children.filter(el => el.name == 'enemyBox')[0].children.filter(el => el.name == 'enemyBoxRight')[0], item)) {
+          element.position.x += element.userData.speed;
+          element.userData.turn = element.userData.angle[randomIntFromInterval(0,3)];
+        };
+        if (item.name.indexOf('Cube0') >= 0 && detectCollisionCubes(element.children.filter(el => el.name == 'enemyBox')[0].children.filter(el => el.name == 'enemyBoxTop')[0], item)) {
+          element.position.z -= element.userData.speed;
+          element.userData.turn = element.userData.angle[randomIntFromInterval(0,3)];
+        };
+        if (item.name.indexOf('Cube0') >= 0 && detectCollisionCubes(element.children.filter(el => el.name == 'enemyBox')[0].children.filter(el => el.name == 'enemyBoxBottom')[0], item)) {
+          element.position.z += element.userData.speed;
+          element.userData.turn = element.userData.angle[randomIntFromInterval(0,3)];
+        };
+
+      
+
+
       
 
     });
+    
 
 
-    //console.log(element.userData.turn);
+    //console.log(element.children.filter(el => el.name == 'enemyBody'));
     
   });
 
 
+}
+
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
